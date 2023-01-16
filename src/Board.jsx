d83e763a69eb9314de4978f1105fbdb479f8fe57
Max <ppmax@bk.ru>
2023-01-16 20:40:55 +0300

    added next turn sub-function

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,14 +5,14 @@ import Square from './Square';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { squares: Array(9).fill(null) };
+    this.state = { squares: Array(9).fill(null), xIsNext: true };
   }
 
   handleClick = (i) => {
-    const { squares } = this.state;
+    const { squares, xIsNext } = this.state;
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
-    this.setState({ squares: newSquares });
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    this.setState({ squares: newSquares, xIsNext: !xIsNext });
   };
 
   renderSquare = (i) => {
@@ -26,7 +26,8 @@ export default class Board extends React.Component {
   };
 
   render() {
-    const status = 'Next player: X';
+    const { xIsNext } = this.state;
+    const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
