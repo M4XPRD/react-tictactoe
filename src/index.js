0e9a2843fe546ff241b40f1eb2ce8fb9c9137737
Max <ppmax@bk.ru>
2023-01-16 21:20:21 +0300

    refactoring to the upper level

--- src/index.js
+++ src/index.js
@@ -2,16 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Board from './Board';
+import calculateWinner from './calculateWinner';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick = (i) => {
+    const { history, xIsNext } = this.state;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares,
+      }]),
+      xIsNext: !xIsNext,
+    });
+  };
+
   render() {
+    const { history, xIsNext } = this.state;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = `"${winner}" has won`;
+    } else {
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
